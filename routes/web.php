15342449caa62d0096625e8440ea54887a14ec22
serikkazy-uly <damirkap89@gmail.com>
2024-03-27 18:45:38 +0500

    feat: add auth

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,29 @@
 <?php
-
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [WelcomeController::class, 'welcome'])->name('welcome');
+
+// Маршруты для аутентификации и регистрации
+Route::get('login', [AuthController::class, 'login'])->name('login');
+Route::post('login', [AuthController::class, 'postLogin'])->name('auth.login'); 
+Route::get('registration', [AuthController::class, 'registration'])->name('register');
+Route::post('register', [AuthController::class, 'postRegistration'])->name('auth.register'); 
+Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('auth.dashboard');
+    })->name('dashboard');
 });
+
+
+
+// Route::get('auth/home', [App\Http\Controllers\Auth\HomeController::class, 'index'])->name('auth.home')->middleware('isAdmin');
+// Route::get('user/home', [App\Http\Controllers\User\HomeController::class, 'index'])->name('user.home');
+
+
+// require __DIR__ . '/auth.php';
